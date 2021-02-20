b4769107e6b242885103963988c059090cd1dd72
Muyoba Lutlas <lutakachihota@gmail.com>
2021-02-20 17:54:32 +0200

    3rd commit

---
+++ src/main/java/io/erehsawsaltul/locationweb/utils/EmailUtil.java
@@ -0,0 +1,5 @@
+package io.erehsawsaltul.locationweb.utils;
+
+public interface EmailUtil {
+	void sendEmail(String toAddress, String subject, String body);
+}
---
+++ src/main/java/io/erehsawsaltul/locationweb/utils/EmailUtilImpl.java
@@ -0,0 +1,15 @@
+package io.erehsawsaltul.locationweb.utils;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailUtilImpl implements EmailUtil {
+
+	//JavaMailSender sender;
+	
+	@Override
+	public void sendEmail(String toAddress, String subject, String body) {
+
+	}
+
+}
